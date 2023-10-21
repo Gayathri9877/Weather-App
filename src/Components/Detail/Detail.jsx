78ecbed57b62ba7f5a69405cdf3d6f13d94f6a62
deshan68 <arundeshan>
2023-10-21 14:54:22 +0530

    add api

--- src/Components/Detail/Detail.jsx
+++ src/Components/Detail/Detail.jsx
@@ -1,99 +1,151 @@
-import React, { useState } from 'react';
-import './Detail.css';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import "./Detail.css";
+import { Link } from "react-router-dom";
 
 export const Detail = () => {
-    const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
 
-    const handleSearch = () => {
-        console.log('Searching for:', searchQuery);
-    };
+  const handleSearch = () => {
+    console.log("Searching for:", searchQuery);
+  };
 
-    return ( 
-        <div className='head' >
-            <div className="center-content">
-                <div className="search-bar">
-                    <input
-                        type="text"
-                        placeholder="City, Latitude and Longitude"
-                        value={searchQuery}
-                        className='Sinput'
-                        onChange={(e) => setSearchQuery(e.target.value)}
-                    />
-                    <button onClick={handleSearch} className='search'>Search</button>
-                </div>
-                <div className="box-container-D">
-                    <div className="box">
-                        <h1>Today</h1>
-                        <form className="form">
-                            <div className="form-group">
-                                <label>Temperature</label>
-                                <input type="text" placeholder="Temperature" />
-                            </div>
-                            <div className="form-group">
-                                <label>Humidity</label>
-                                <input type="text" placeholder="Humidity" />
-                            </div>
-                            <div className="form-group">
-                                <label>Wind</label>
-                                <input type="text" placeholder="Wind" />
-                            </div>
-                            <div className="form-group">
-                                <label>Status</label>
-                                <input type="text" placeholder="Status" />
-                            </div>
-                        </form>
-                    </div>
-                    <div className="box">
-                        <h1>Tomorrow</h1>
-                        <form className="form">
-                            <div className="form-group">
-                                <label>Temperature</label>
-                                <input type="text" placeholder="Temperature" />
-                            </div>
-                            <div className="form-group">
-                                <label>Humidity</label>
-                                <input type="text" placeholder="Humidity" />
-                            </div>
-                            <div className="form-group">
-                                <label>Wind</label>
-                                <input type="text" placeholder="Wind" />
-                            </div>
-                            <div className="form-group">
-                                <label>Status</label>
-                                <input type="text" placeholder="Status" />
-                            </div>
-                        </form>
-                    </div>
-                    <div className="box">
-                        <h1>Day After Tomorrow</h1>
-                        <form className="form">
-                            <div className="form-group">
-                                <label>Temperature</label>
-                                <input type="text" placeholder="Temperature" />
-                            </div>
-                            <div className="form-group">
-                                <label>Humidity</label>
-                                <input type="text" placeholder="Humidity" />
-                            </div>
-                            <div className="form-group">
-                                <label>Wind</label>
-                                <input type="text" placeholder="Wind" />
-                            </div>
-                            <div className="form-group">
-                                <label>Status</label>
-                                <input type="text" placeholder="Status" />
-                            </div>
-                        </form>
-                    </div>
-                </div>
-                <div>
-                <Link to="/MDetails">
-                    <button type="submit"  className='seem'>See more</button>
-                </Link>
-                    
-                </div>
-            </div>
+  useEffect(() => {
+    // Fetch data from the API and set it to the state
+    const apiUrl =
+      "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=2e1d8a39fc5dcbee1dee351a038df739";
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        setWeatherData(data);
+        console.log(data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  if (!weatherData) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="head">
+      <div className="center-content">
+        <div className="search-bar">
+          <input
+            type="text"
+            placeholder="City, Latitude and Longitude"
+            value={searchQuery}
+            className="Sinput"
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+          <button onClick={handleSearch} className="search">
+            Search
+          </button>
         </div>
-    );
+        <div className="box-container">
+          <div className="box">
+            <h1>Today</h1>
+            {/* ------- */}
+            <form className="form">
+              <span>
+                Current Weather in {weatherData.name}, {weatherData.sys.country}
+              </span>
+              <div className="form-group">
+                <label>Temperature</label>
+                <input
+                  type="text"
+                  placeholder="Temperature"
+                  value={weatherData.main.temp}
+                  readOnly
+                />
+              </div>
+              <div className="form-group">
+                <label>Humidity</label>
+                <input
+                  type="text"
+                  placeholder="Humidity"
+                  value={weatherData.main.humidity}
+                  readOnly
+                />
+              </div>
+              <div className="form-group">
+                <label>Wind</label>
+                <input
+                  type="text"
+                  placeholder="Wind"
+                  value={weatherData.wind.speed}
+                  readOnly
+                />
+              </div>
+              <div className="form-group">
+                <label>Status</label>
+                <input
+                  type="text"
+                  placeholder="Status"
+                  value={weatherData.weather[0].main}
+                  readOnly
+                />
+              </div>
+              <div className="form-group">
+                <label>Description</label>
+                <input
+                  type="text"
+                  placeholder="Status"
+                  value={weatherData.weather[0].description}
+                  readOnly
+                />
+              </div>
+            </form>
+          </div>
+          {/* <div className="box">
+            <h1>Tomorrow</h1>
+            <form className="form">
+              <div className="form-group">
+                <label>Temperature</label>
+                <input type="text" placeholder="Temperature" />
+              </div>
+              <div className="form-group">
+                <label>Humidity</label>
+                <input type="text" placeholder="Humidity" />
+              </div>
+              <div className="form-group">
+                <label>Wind</label>
+                <input type="text" placeholder="Wind" />
+              </div>
+              <div className="form-group">
+                <label>Status</label>
+                <input type="text" placeholder="Status" />
+              </div>
+            </form>
+          </div>
+          <div className="box">
+            <h1>Day After Tomorrow</h1>
+            <form className="form">
+              <div className="form-group">
+                <label>Temperature</label>
+                <input type="text" placeholder="Temperature" />
+              </div>
+              <div className="form-group">
+                <label>Humidity</label>
+                <input type="text" placeholder="Humidity" />
+              </div>
+              <div className="form-group">
+                <label>Wind</label>
+                <input type="text" placeholder="Wind" />
+              </div>
+              <div className="form-group">
+                <label>Status</label>
+                <input type="text" placeholder="Status" />
+              </div>
+            </form>
+          </div> */}
+        </div>
+        <div>
+          <Link to="MDetails">
+            <button className="seem">See more</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/Components/MDetails/MDetails.jsx
+++ src/Components/MDetails/MDetails.jsx
@@ -1,167 +1,66 @@
-import React from 'react'
-import './MDetails.css';
-
+import React, { useEffect, useState } from "react";
+import "./MDetails.css";
 
 export const MDetails = () => {
-    return ( 
-    <div className='head' >
-        <div>
-            <h1 className='wh'>Weather Forecast for the Whole Week</h1>
-        </div>
-        <div className="box-container">
-                    <div className="box">
-                        <div><img src="./Assets/Images/sunny-day.png" alt="" srcset="" /></div>
-                        <h1>Monday</h1>
-                        <form className="form">
-                            <div className="form-group">
-                                <label className='l1'>Temperature</label>
-                                <input type="text" placeholder="Temperature" />
-                            </div>
-                            <div className="form-group">
-                                <label>Humidity</label>
-                                <input type="text" placeholder="Humidity" />
-                            </div>
-                            <div className="form-group">
-                                <label>Wind</label>
-                                <input type="text" placeholder="Wind" />
-                            </div>
-                            <div className="form-group">
-                                <label>Status</label>
-                                <input type="text" placeholder="Status" />
-                            </div>
-                        </form>
-                    </div>
-                    <div className="box">
-                        <h1>Tuesday</h1>
-                        <form className="form">
-                            <div className="form-group">
-                                <label>Temperature</label>
-                                <input type="text" placeholder="Temperature" />
-                            </div>
-                            <div className="form-group">
-                                <label>Humidity</label>
-                                <input type="text" placeholder="Humidity" />
-                            </div>
-                            <div className="form-group">
-                                <label>Wind</label>
-                                <input type="text" placeholder="Wind" />
-                            </div>
-                            <div className="form-group">
-                                <label>Status</label>
-                                <input type="text" placeholder="Status" />
-                            </div>
-                        </form>
-                    </div>
-                    <div className="box">
-                        <h1>Wednesday</h1>
-                        <form className="form">
-                            <div className="form-group">
-                                <label>Temperature</label>
-                                <input type="text" placeholder="Temperature" />
-                            </div>
-                            <div className="form-group">
-                                <label>Humidity</label>
-                                <input type="text" placeholder="Humidity" />
-                            </div>
-                            <div className="form-group">
-                                <label>Wind</label>
-                                <input type="text" placeholder="Wind" />
-                            </div>
-                            <div className="form-group">
-                                <label>Status</label>
-                                <input type="text" placeholder="Status" />
-                            </div>
-                        </form>
-                    </div>
-                    <div className="box">
-                        <h1>Thursday</h1>
-                        <form className="form">
-                            <div className="form-group">
-                                <label>Temperature</label>
-                                <input type="text" placeholder="Temperature" />
-                            </div>
-                            <div className="form-group">
-                                <label>Humidity</label>
-                                <input type="text" placeholder="Humidity" />
-                            </div>
-                            <div className="form-group">
-                                <label>Wind</label>
-                                <input type="text" placeholder="Wind" />
-                            </div>
-                            <div className="form-group">
-                                <label>Status</label>
-                                <input type="text" placeholder="Status" />
-                            </div>
-                        </form>
-                    </div>
-                    <div className="box">
-                        <h1>Friday</h1>
-                        <form className="form">
-                            <div className="form-group">
-                                <label>Temperature</label>
-                                <input type="text" placeholder="Temperature" />
-                            </div>
-                            <div className="form-group">
-                                <label>Humidity</label>
-                                <input type="text" placeholder="Humidity" />
-                            </div>
-                            <div className="form-group">
-                                <label>Wind</label>
-                                <input type="text" placeholder="Wind" />
-                            </div>
-                            <div className="form-group">
-                                <label>Status</label>
-                                <input type="text" placeholder="Status" />
-                            </div>
-                        </form>
-                    </div>
-                    <div className="box">
-                        <h1>Saturday</h1>
-                        <form className="form">
-                            <div className="form-group">
-                                <label>Temperature</label>
-                                <input type="text" placeholder="Temperature" />
-                            </div>
-                            <div className="form-group">
-                                <label>Humidity</label>
-                                <input type="text" placeholder="Humidity" />
-                            </div>
-                            <div className="form-group">
-                                <label>Wind</label>
-                                <input type="text" placeholder="Wind" />
-                            </div>
-                            <div className="form-group">
-                                <label>Status</label>
-                                <input type="text" placeholder="Status" />
-                            </div>
-                        </form>
-                    </div>
-                    <div className="box">
-                        <h1>Sunday</h1>
-                        <form className="form">
-                            <div className="form-group">
-                                <label>Temperature</label>
-                                <input type="text" placeholder="Temperature" />
-                            </div>
-                            <div className="form-group">
-                                <label>Humidity</label>
-                                <input type="text" placeholder="Humidity" />
-                            </div>
-                            <div className="form-group">
-                                <label>Wind</label>
-                                <input type="text" placeholder="Wind" />
-                            </div>
-                            <div className="form-group">
-                                <label>Status</label>
-                                <input type="text" placeholder="Status" />
-                            </div>
-                        </form>
-                    </div>
-                </div>
-        
+  const [weatherData, setWeatherData] = useState([]);
+
+  useEffect(() => {
+    // Fetch data from the API and set it to the state
+    const apiUrl =
+      "https://api.openweathermap.org/data/2.5/forecast?lat=6.927079&lon=79.861244&units=metric&exclude={hourly}&appid=2e1d8a39fc5dcbee1dee351a038df739";
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        setWeatherData(data.list);
+        console.log(data);
+        makeNewArray(data?.list);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  function makeNewArray(data) {
+    console.log(data);
+    const newArray = data.reduce((result, item) => {
+      let entryData = item.dt_txt;
+      const dateRegex = /^(\d{4}-\d{2}-\d{2})/; // Match the date portion
+      const match = entryData.match(dateRegex);
+      if (match) {
+        const extractedDate = match[1]; // The matched date portion
+        console.log(extractedDate);
+      } else {
+        console.log("No date found in the string.");
+      }
+    }, {});
+  }
+
+  return (
+    <div className="head">
+      <div className="box-container">
+        {weatherData.slice(0, 3).map((item, index) => (
+          <div key={index} className="box">
+            <h1>Date: {item.dt_txt}</h1>
+            <form className="form">
+              <div className="form-group">
+                <label>Temperature</label>
+                <input type="text" placeholder="Temperature" />
+              </div>
+              <div className="form-group">
+                <label>Humidity</label>
+                <input type="text" placeholder="Humidity" />
+              </div>
+              <div className="form-group">
+                <label>Wind</label>
+                <input type="text" placeholder="Wind" />
+              </div>
+              <div className="form-group">
+                <label>Status</label>
+                <input type="text" placeholder="Status" />
+              </div>
+            </form>
+          </div>
+        ))}
+      </div>
+      <div></div>
     </div>
-    
-);
-   
+  );
 };
-  
